586eaa82571aab15d54d7e359c3e39dbaeebff7d
tisuchi <tisuchi@gmail.com>
2019-08-24 18:04:02 +0600

    Put everything together

--- factory-pattern/Automobile.php
+++ factory-pattern/Automobile.php
@@ -11,8 +11,22 @@ class Automobile
 		$this->vehicleModel = $model;
 	}
 
-	public function getMakeAnModel()
+	public function getMakeAndModel()
 	{
 		return $this->vehicleMake . ' ' . $this->vehicleModel;
 	}
 }
+
+
+class AutomobileFactory
+{
+	public static function create($make, $model)
+	{
+		return new Automobile($make, $model);
+	}
+}
+
+// Having the factory to create the automobile. 
+$volkswagen = AutomobileFactory::create("volkswagen", "GIT2");
+
+print_r($volkswagen->getMakeAndModel());
